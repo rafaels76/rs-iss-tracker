4ac6215eeeefd0cfa341e2fe342aa38c5d1e4ef9
Rafael Sanchez <rafaels76@gmail.com>
2022-04-05 21:43:14 -0400

    Implementación de iconos día/noche

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,10 @@ var map = L.map("map").setView([0, 09], 2);
 var currentVisibility = '';
 const issIconDayLight = L.icon({iconUrl: "assets/img/iss.png", iconSize: [50, 50], iconAnchor: [25, 25]});
 const issIconEclipsed = L.icon({iconUrl: "assets/img/iss-e.png", iconSize: [50, 50], iconAnchor: [25, 25] });
-
 const issRadious = L.circle([0,0], 2200e3, {color: "#19630a", opacity: 0.3, weight:1, fillColor: "#19630a", fillOpacity: 0.1}).addTo(map); 
 const issMarker = L.marker([0, 0], { icon: issIconDayLight }).addTo(map);
-const url = `https://api.wheretheiss.at/v1/satellites/25544`;
+const urlISS = `https://api.wheretheiss.at/v1/satellites/25544`;
+const urlLeaflet = "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}";
 let firstTime = true;
 
 var t = L.terminator();
@@ -13,27 +13,27 @@ t.addTo(map);
 
 
 L.tileLayer(
-    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+    urlLeaflet,
     {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
       maxZoom: 18,
       id: "mapbox/streets-v11",
       tileSize: 512,
       zoomOffset: -1,
-      accessToken: "pk.eyJ1IjoicmFmYWVsczc2IiwiYSI6ImNrc3FyMzQ2ajBmb2Myb3FrMzV1YjhpdDEifQ.Iy0ikuINGfu5dStC-nwIow"
+      accessToken: config.API_TOKEN
     }
   ).addTo(map);
 
 
 
 async function getISS() {
-    const response = await fetch(url);
+    const response = await fetch(urlISS);
     const data = await response.json();
     
     const { altitude, latitude, longitude, velocity, visibility } = data;
     issRadious.setLatLng([latitude, longitude]);
     issMarker.setLatLng([latitude, longitude]);
-    // Set location of ISS on the map
+    // Establecer ubicacopn de la ISS en el mapa
     map.setView([latitude, longitude]);
     // Check if is the first load of the page
     if (firstTime) {
@@ -48,7 +48,7 @@ async function getISS() {
         document.getElementById('labelDayNight').innerText = 'La ISS está a la sombra de la Tierra';
       }
     }
-    //Put light or dark icon depending on daylight
+    //Establecer el icono de la estacion segun se dia o noche
     if(currentVisibility != visibility){
       currentVisibility = visibility;
       if(visibility == 'daylight'){
@@ -59,7 +59,7 @@ async function getISS() {
         document.getElementById('labelDayNight').innerText = 'La ISS está a la sombra de la Tierra';
       }
     }
-    // Refresh the labels
+    // Actualizar las labels con las coordenadas
     document.getElementById('labelLatitude').innerText = latitude.toFixed(6);
     document.getElementById('labelLongitude').innerText = longitude.toFixed(6);
     document.getElementById('labelAltitude').innerText = altitude.toFixed(2);
